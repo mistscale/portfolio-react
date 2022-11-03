3c0dfb5b3dbd311c829522baa72c8d482e19764e
Lisen Lundgren <lisolundgren@gmail.com>
2022-11-03 17:22:44 +0100

    Import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import Projects from './components/Projects';
+import Header from './components/Header';
+import About from './components/About';
+import FeaturedProjects from './components/FeaturedProjects';
+import OtherProjects from './components/OtherProjects';
 
 const App = () => {
 	return (
-		<div className='container'>
-			<Projects />
-		</div>
+		<>
+			<Header />
+			<About />
+			<FeaturedProjects />
+			<OtherProjects />
+		</>
 	);
 };
 
